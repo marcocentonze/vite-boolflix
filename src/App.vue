f2bcc4655e9cf941da9934cac654be3a6867cbcc
marcocentonze <marco.centonze27@gmail.com>
2023-09-28 12:29:03 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+    name: 'App',
+    components: {
+        HelloWorld
+    },
+    data() {
+        return {
+            
+        };
+    },
+};
+
+
+
+
+
+
+
+</script>
+
+<template>
+  
+
+  <HelloWorld/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<script >
+export default {
+    name: 'HelloWorld',
+    components: {
+        
+    },
+    data() {
+        return {
+            
+        };
+    },
+};
+
+
+
+</script>
+
+<template>
+    <h1 class="text-center text-danger">prova</h1>
+</template>
+
+<style scoped></style>
