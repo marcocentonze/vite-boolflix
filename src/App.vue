30c5ad8a12a9934ee564baac04f2f1764bc5952c
marcocentonze <marco.centonze27@gmail.com>
2023-10-01 18:47:21 +0200

    style markup + hover on cards

--- src/App.vue
+++ src/App.vue
@@ -43,74 +43,62 @@ export default {
 
 <template>
     <HelloWorld />
-    <!--------------------------------- NAVBAR -------------------------->
-    <div class="bg-container py-3 container-fluid">
-    <div class="row align-items-center">
-        <div class="col-md-3 text-md-start mb-3">
-            <img src="https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png" class="img-fluid" alt="Logo">
+    <!-- navbar -->
+    <div class="text-center bg-container d-flex py-3 container-fluid">
+        <div class="col-4">
+            <img src="https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png">
         </div>
-        <div class="col-md-9 text-md-end">
-            <form class="d-flex">
-                <input type="search" class="form-control me-2" placeholder="Cerca i titoli di serie tv e film..." v-model="movieSearch" @keyup.enter="searchResult()">
-                <button type="submit" class="btn btn-primary" style="background-color: #E50914; border-color: #E50914;">Cerca</button>
-            </form>
+
+        <div class="col-4 pt-5">
+            <a class="px-2 text-decoration-none text-black" href="#">FILM</a>
+            <a class="px-2 text-decoration-none text-black" href="#">FILM</a>
+            <a class="px-2 text-decoration-none text-black" href="#">FILM</a>
+            <a class="px-2 text-decoration-none text-black" href="#">FILM</a>
+        </div>
+
+        <div class="col-4 pt-4">
+            <input class="mx-2" type="search" placeholder="Cerca il tuo film..." v-model="movieSearch"
+                @keyup.enter="searchResult()">
+            <button type="submit" @click="searchResult()">Cerca</button>
         </div>
     </div>
-</div>
 
 
-  <!-------------------------------------MAIN----------------------------->
+
+    <!--MAIN-->
     <main class="bg-container">
-        <!-- results -->
-        <div class="container mt-4">
-            <div class="row g-4">
-                <div class="col-md-4" v-for="movie in store.movies">
+    <div class="container mt-4">
+        <div class="row g-4">
+            <div class="col-md-4" v-for="movie in store.movies">
+                <div class="card movie-card">
+                    
+                    <div class="card-image rounded" :style="'background-image: url(https://image.tmdb.org/t/p/w342' + movie.poster_path + ')'"></div>
                     <ul class="list-group">
-                        <!-- TO DO:creare classe a poster_path per stylizzarlo -->
-                        <img :src="'https://image.tmdb.org/t/p/w342' + movie.poster_path">
                         <li class="list-group-item bg-color-card" v-if="movie.media_type != 'person'">
-
-                            <span v-if="movie.media_type == 'movie'">TITOLO: {{ movie.title }}
-                            </span>
-
-                            <span v-if="movie.media_type == 'tv'">TITOLO: {{ movie.name }}
-
-                            </span>
+                            <div v-if="movie.media_type == 'movie'">TITOLO: {{ movie.title }}</div>
+                            <div v-if="movie.media_type == 'tv'">TITOLO: {{ movie.name }}</div>
+                            <div>TITOLO ORIGINALE: {{ movie.original_title ?? movie.original_name }}</div>
+                            <div>LINGUA:
+                                <img :src="languageFlags[movie.original_language]" alt="Flag" v-if="languageFlags[movie.original_language]">
+                                <span v-else>Lingua sconosciuta</span>
+                            </div>
+                            <div>TRAMA: {{ movie.overview }}</div>
+                            <div>VOTO:</div>
+                            <div class="mb-2">
+                                <span v-for="star in 5">
+                                    <i :class="['fa', 'fa-star', 'text-warning', star <= Math.ceil(movie.vote_average / 2) ? 'fa-solid' : 'fa-regular']"></i>
+                                </span>
+                                
+                            </div>
                         </li>
-
-                        <li class="list-group-item bg-color-card">
-                            <span v-if="movie.media_type == 'movie'">TITOLO ORIGINALE: {{ movie.original_title }}
-                            </span>
-
-                            <span v-if="movie.media_type == 'tv' || movie.media_type == 'person'">TITOLO ORIGINALE:
-                                {{ movie.original_name }}
-                            </span>
-                        </li>
-
-                        <li class="list-group-item bg-color-card" v-if="movie.media_type != 'person'">LINGUA:
-                            <span v-if="languageFlags[movie.original_language]">
-                                <img :src="languageFlags[movie.original_language]" alt="Flag">
-                            </span>
-
-                            <span v-else>
-                                Lingua sconosciuta
-                            </span>
-                        </li>
-
-                        <li class="list-group-item bg-color-card" v-if="movie.media_type != 'person'">
-                            <span>VOTO:</span>
-                            <span v-for="star in 5">
-                                <!---genero 5 stelle,condizione con match.ceil e solid(piena) o regular(vuota)-->
-                                <i
-                                    :class="['fa', 'fa-star', 'text-warning', star <= Math.ceil(movie.vote_average / 2) ? 'fa-solid' : 'fa-regular']"></i>
-                            </span>
-                        </li>
-
                     </ul>
                 </div>
             </div>
         </div>
-    </main>
+    </div>
+</main>
+
+
 </template>
 
 <style scoped>
@@ -122,7 +110,38 @@ export default {
     background-color: rgb(255, 245, 225);
     ;
 }
+
+.card.movie-card .card-image {
+    width: 100%;
+    height: 500px;
+    background-size: cover;
+    transition: filter 0.3s, transform 0.3s;
+}
+
+/* Stile info */
+.card.movie-card .list-group-item {
+    display: none;
+    background-color: rgba(255, 255, 255, 0.9);
+    position: absolute;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+}
+
+
+.card.movie-card:hover .list-group-item {
+    display: flex;
+    flex-direction: column;
+}
 </style>
 
 <!-- notes:
-I used multi search on themoviedb and "Use multi search when you want to search for movies, TV shows and people in a single request."- So i have to delete "people" from the search con IF -->
+-formula (long form)= <i v-for="i in Math.ceil(movie.vote_average / 2)" class="fa-solid fa-star"></i>
+                    <i v-for="i in (5 - Math.ceil((movie.vote_average / 2)))" class="fa-regular fa-star" v-if="movie.vote_average != 0"></i>
+                    <i v-for="i in 5" class="fa-regular fa-star" v-if="movie.vote_average == 0"></i>
+
+-    I used multi search on themoviedb and "Use multi search when you want to search for movies, TV shows and people in a single request."- So i have to delete "people" from the search con IF -->
